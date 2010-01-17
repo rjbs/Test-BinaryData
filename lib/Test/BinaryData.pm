fae5d277f8d50c2b129ff49dde7c177cbb27d3e3
Ricardo Signes <rjbs@cpan.org>
2010-01-16 21:29:29 -0500

    update prereqs

--- lib/Test/BinaryData.pm
+++ lib/Test/BinaryData.pm
@@ -10,11 +10,11 @@ Test::BinaryData - compare two things, give hex dumps if they differ
 
 =head1 VERSION
 
-version 0.009
+version 0.010
 
 =cut
 
-our $VERSION = '0.009';
+our $VERSION = '0.010';
 
 =head1 SYNOPSIS
 
